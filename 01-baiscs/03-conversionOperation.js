cfa83f2b0b7e4f6dc490f478fa7bf3562a5d60d0
AK11RAWAT <abhishekrawat1104@gmail.com>
2025-07-25 16:55:52 +0000

    conversion Operation

---
+++ 01-baiscs/03-conversionOperation.js
@@ -0,0 +1,49 @@
+//let score = ""
+//console.log(typeof score);//
+//console.log(typeof (score));//
+
+//let valueInNumber = Number(score);
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+//"33"=>33
+//"33abd"=>NaN
+//null=>0
+//true=>1;false=>0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=>true;0=>false
+//""=>false
+//"hitesh"=>true
+
+//*  * * * * * * * * * * * * * * * * * * Operation   * * * * * * * * * * * * * * * * ** 
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " world "
+let str3 = str1+str2
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);//if string is first so all will be treated as string 
+//console.log(1+2+"2");//if string is in last first conversion will happen then string will get attach
+
+console.log(+true);
+console.log(true);
+console.log(+"");
+
+let gamecounter = 100
+gamecounter ++;
+console.log(gamecounter);
