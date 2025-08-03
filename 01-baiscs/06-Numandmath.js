72e118d9036af68da4a5a998ce80edae94a06f73
AK11RAWAT <abhishekrawat1104@gmail.com>
2025-08-03 15:01:12 +0000

    num and math

---
+++ 01-baiscs/06-Numandmath.js
@@ -0,0 +1,35 @@
+const score = Number(400);
+console.log (score);
+const balance = new Number (100);
+console.log(balance);
+console.log(balance.toString());
+console. log (balance.toString.length);
+
+
+console.log(balance.toFixed(2));//to fixed will give point after value how many point it will give will decided 
+//by the no you put inside the bracket if the no is 2 it will give two number after point and if the no is 
+//3 it will give three number after point .
+
+
+const otherNumber = 23.55553
+console.log(otherNumber.toPrecision(3));
+//it will give value rill three no before point included so the output will be 23.6
+//it will also round off the no that is present as you can see that the no given was 23.55 and now if you look at the 
+//output the output is the round of 23.55 .
+const otheNumber = 123.896
+console.log (otheNumber.toPrecision(3));
+//it will round of the no
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString());
+// it will give comas it by default will give you in US standard 
+//but if you weite in indian standard just write this in bracket ('en-IN').
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//***************************   FOR DSA ****************************************************** ******************/
+
+/*max-value - maximum value kitna  tak ja sakti ha 
+min value - minmum value kitna tak ja sakti ha*/
+
